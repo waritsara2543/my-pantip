aefafc50de60875721a314adb15d164eddc607f8
waritsara <warissara.0039@gmail.com>
2024-04-08 17:37:03 +0700

    add not found blog

--- app/components/BlogList.tsx
+++ app/components/BlogList.tsx
@@ -7,16 +7,22 @@ const BlogList = () => {
   const blog = useAppSelector((state) => state.blog.filteredBlogs);
 
   return (
-    <div className="pt-10 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-10 gap-5">
-      {blog.map((item) => (
-        <BlogCard
-          key={item.id}
-          title={item.title}
-          img={item.image || "/logo.svg"}
-          tag={item.tags}
-          room={item.room}
-        />
-      ))}
+    <div className="pt-10">
+      {blog.length === 0 ? (
+        <div className="text-center text-2xl font-bold">No Blogs Found</div>
+      ) : (
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-10 gap-5">
+          {blog.map((item) => (
+            <BlogCard
+              key={item.id}
+              title={item.title}
+              img={item.image || "/logo.svg"}
+              tag={item.tags}
+              room={item.room}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
